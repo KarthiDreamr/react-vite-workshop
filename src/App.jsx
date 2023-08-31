ca4c6f6decc370ee05c7569b854155ccbaeb85b1
KarthiDreamr <karthidreamr@gmail.com>
2023-08-31 09:41:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './Components/FunctionComponent'
+import ClassComponent from './Components/ClassComponent'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      <ClassComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/FunctionComponent.jsx
@@ -0,0 +1,10 @@
+function FunctionComponent(){
+    return(
+        <div>
+            <h1>Function Component Export</h1>
+        </div>
+    );
+}
+
+export default FunctionComponent;
+
