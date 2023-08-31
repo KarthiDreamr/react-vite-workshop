ea2ea629472ae222c955c43b6641d0f6bfd34b07
KarthiDreamr <karthidreamr@gmail.com>
2023-08-31 12:32:48 +0530

    added input box with attribute

--- src/Components/ClassComponent.jsx
+++ src/Components/ClassComponent.jsx
@@ -4,26 +4,44 @@ class ClassComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: "Class Component" // set the title property in state
+            title: "Class Component",
+            user: ""
         };
     }
 
 
-    changeValue(){
-        this.setState({title:"Class Component Modified"})
+    changeValue() {
+        this.setState({ title: "Class Component Modified" })
     }
-        
+
+    changeListerner(event) {
+        console.log("Input value changed")
+
+        this.setState({
+            title: this.state.title,
+            user: event.target.value,
+        }
+
+        )
+    }
+
 
     render() {
         return (
             <div>
+                {/* <h1>{this.props.title}</h1>                 */}
                 <h1>{this.state.title}</h1>
                 <h2>Class Component Export</h2>
-                <button on onClick={()=>this.changeValue()}>click to modify</button>
-
+                <h1>User = {this.state.user}</h1>
+                <button onClick={() => this.changeValue()}>click to modify</button>
+                <br></br>
+                <br></br>
+                <input onChange={(event) => this.changeListerner(event)}></input>
             </div>
         );
     }
 }
 
 export default ClassComponent;
+
+
