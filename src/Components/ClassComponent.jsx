307978915772474a583087a3f62c342fc04accda
Hemarubini <hemashahas316@gmail.com>
2023-08-31 10:22:27 +0530

    modified props title

--- src/Components/ClassComponent.jsx
+++ src/Components/ClassComponent.jsx
@@ -1,8 +1,17 @@
 import React,{Component} from "react";
 
 class ClassComponent extends Component{
+
+    constructor(props)
+    {
+        this.props={
+            title:"Class Component"
+        }
+
+    }
+
     render(){
-        this.props={"title":"Class Component"}
+        // this.props={"title":"Class Component"}
         return(
             <div>
                 <h1>{this.props.title}</h1>
